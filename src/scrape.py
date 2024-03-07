d34051505876f018957e7498857e422c7e55a8db
Cooper Pflaum <cooperpflaum@gmail.com>
2024-03-07 15:07:17 -0500

    Added url generation for the instagram url

--- src/scrape.py
+++ src/scrape.py
@@ -13,10 +13,10 @@ from rich import print
 from rich.progress import Progress
 from rich.progress import track
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 
 
@@ -25,22 +25,24 @@ import chromedriver_autoinstaller
 
 
 def find_user(name, driver):
-    name = name.replace(' ', '%20')
-    url = f'https://www.picuki.com/search/{name}'
-    driver.delete_all_cookies();
+    if '@' not in name:
+        name = name.replace(' ', '%20')
+        url = f'https://www.picuki.com/search/{name}'
+        driver.delete_all_cookies();
 
 
-    driver.get(url)
+        driver.get(url)
 
-    wait = WebDriverWait(driver, 10)
-    element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.search-result-box")))
-    driver.execute_script("window.stop();")
-    # Use BeautifulSoup to parse the HTML
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    usernames = [username.text.strip() for username in soup.find_all('div', class_='result-username')]
-    for uname in usernames:
-        scrape_user_data(uname, driver)
-
+        wait = WebDriverWait(driver, 10)
+        element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.search-result-box")))
+        driver.execute_script("window.stop();")
+        # Use BeautifulSoup to parse the HTML
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        usernames = [username.text.strip() for username in soup.find_all('div', class_='result-username')]
+        for uname in usernames:
+            scrape_user_data(uname, driver)
+    else:
+        scrape_user_data(name, driver)
 
 
 def scrape_user_data(username, driver, update=True):
@@ -104,6 +106,8 @@ def scrape_user_data(username, driver, update=True):
             'Following': soup.find('div', class_='item_following').find('div', class_='num').text.strip() if soup.find('div', class_='item_following') else None,
             'Private':                                                                                    True if soup.find('div', class_='notice') else False,
             'Verified':                                                                                   True if soup.find('span', class_='ident verified icon icon_verified') else False,
+            'URL':       url                                                                              if soup.find('div', class_='username') else None,
+            'InstaURL':  f'https://www.instagram.com/{soup.find("div", class_="username").text.strip().replace("@", "")}/' if soup.find('div', class_='username') else None,
         }
 
 
@@ -139,25 +143,15 @@ async def process_post_data(post, post_count, save_dir):
     post_dir = os.path.join(save_dir, f'post_{post_count}')
     os.makedirs(post_dir, exist_ok=True)
 
-    download_urls = [a.get('href') for a in post.find_all('a') if not a.get('href').startswith('/')]
-    description = post.find('div', class_='sum').get_text(strip=True) if post.find('div', class_='sum') else "no description"
-    likes = post.find('span', class_='count_item_like').get_text().replace('\n', '') if post.find('span', class_='count_item_like') else post.find('div', class_='count_item_like').get_text().replace('\n', '')
-    comments = post.find('span', class_='count_item_comment').get_text().replace('\n', '') if post.find('span', class_='count_item_comment') else post.find('div', class_='count_item_comment').get_text().replace('\n', '')
-    date = post.find('div', class_='time').get_text().replace('\n', '').replace('\n', '')
-    hashtags = [tag.get_text() for tag in post.find('div', class_='sum').find_all('a') if tag.get_text().startswith('#')]
-    mentions = [tag.get_text() for tag in post.find('div', class_='sum').find_all('a') if tag.get_text().startswith('@')]
-
     post_data = {
-        'urls': download_urls,
-        'description': description,
-        'likes': likes,
-        'comments': comments,
-        'time_posted': date,
-        'hashtags': hashtags,
-        'mentions': mentions,
+        'urls' :       [a.get('href') for a in post.find_all('a')                                      if not a.get('href').startswith('/')],
+        'description': post.find('div',  class_='sum').get_text(strip=True)                            if post.find('div', class_='sum')                 else "no description",
+        'likes':       post.find('span', class_='count_item_like').get_text().replace('\n', '')        if post.find('span', class_='count_item_like')    else post.find('div', class_='count_item_like').get_text().replace('\n', ''),
+        'comments':    post.find('span', class_='count_item_comment').get_text().replace('\n', '')     if post.find('span', class_='count_item_comment') else post.find('div', class_='count_item_comment').get_text().replace('\n', ''),
+        'time_posted': post.find('div',  class_='time').get_text().replace('\n', '').replace('\n', ''),
+        'hashtags':    [tag.get_text() for tag in post.find('div', class_='sum').find_all('a') if tag.get_text().startswith('#')],
+        'mentions':    [tag.get_text() for tag in post.find('div', class_='sum').find_all('a') if tag.get_text().startswith('@')],
     }
-    print(f'Post {post_count + 1}\n{post_data}\n')
-
     await download_images(download_urls, post_dir)
 
 
@@ -166,11 +160,12 @@ async def scrape_all_posts(parsed_posts, save_dir):
     for post_count, post in enumerate(parsed_posts):
         task = asyncio.create_task(process_post_data(post, post_count, save_dir))
         tasks.append(task)
-
     await asyncio.gather(*tasks)
 
-def scrape_instagram_posts(username, driver):
 
+
+
+def scrape_instagram_posts(username, driver):
     # Load Instagram page
     url = f'https://www.pixwox.com/profile/{username}/'
 
@@ -199,7 +194,9 @@ def scrape_instagram_posts(username, driver):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 # Find all the div elements with class 'post_box be-1'
+# Anything with this element I can consider to be a post
     posts = soup.find_all('div', class_='post_box be-1')
+
 # Loop through each post
     save_dir = f'../captured_users/{username}/posts'
     os.makedirs(save_dir, exist_ok=True)
